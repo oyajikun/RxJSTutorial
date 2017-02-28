d7a4e58f7a1b5db20bf558c2c4048a78bdfaaba3
oyajikun <oyajikun>
2017-02-28 22:11:31 +0900

    First Rx

--- src/index.js
+++ src/index.js
@@ -3,5 +3,15 @@
  */
 
 /* eslint-disable no-console */
+import Rx from 'rx';
+
 const str = 'ES6';
 console.log(`Hello ${str}`);
+
+const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+const source = Rx.Observable.fromArray(array);
+
+const filtered = source.filter(x => x % 3 === 0);
+
+filtered.subscribe(x => console.log(x));
