ee68d92e10fe844b83ea53343444d1fc43f12872
oyajikun <oyajikun>
2017-03-06 22:33:43 +0900

    Reallocate const value

--- src/index.js
+++ src/index.js
@@ -16,9 +16,7 @@ const filtered = source.filter(x => x % 3 === 0);
 
 filtered.subscribe(x => console.log(x));
 
-const mapped = source.map((x) => {
-  const ret = ['-', 'いち', 'に', 'さん', 'よん', 'ご', 'ろく', 'なな', 'はち', 'きゅう', 'じゅう'];
-  return ret[x];
-});
+const ret = ['-', 'いち', 'に', 'さん', 'よん', 'ご', 'ろく', 'なな', 'はち', 'きゅう', 'じゅう'];
+const mapped = source.map(x => ret[x]);
 
 mapped.subscribe(x => console.log(x));
